172d11d32c53149b079ac1649dc8ec391fd67994
周庆10229428 <zhou.qing4@zte.com.cn>
2025-01-23 16:10:05 +0800

    [优化] 优化CI编译的docker脚本
    Change-Id: I241dc498d5318e2105415ce72947e086ad5e12be

--- build/docker/start-docker.sh
+++ build/docker/start-docker.sh
@@ -7,26 +7,16 @@ ngVersion=$3
 home=$(cd `dirname $0`/../..; pwd);
 cd $home
 
+if [ ! -f "$AWADE_BUILD_RESOURCE/docker/common.sh" ]; then
+    echo "错误: common.sh 文件不存在 ($AWADE_BUILD_RESOURCE/docker/common.sh)"
+    exit 1
+fi
+. $AWADE_BUILD_RESOURCE/docker/common.sh
+
 dockerName="jigsaw_build_${change}_${patch}_${ngVersion}"
 echo "start-docker >>>>>>>>>> dockerName: $dockerName"
 
-cleanupDocker() {
-    echo "cleanupDocker >>>>>>>>>> $dockerName ..."
-    # 停止并删除容器
-    containerId=$(docker ps -a | grep $dockerName | awk {'print $1'})
-    if [ ! -z "$containerId" ]; then
-        echo "$dockerName: 停止并删除容器 $containerId ..."
-        docker kill $containerId
-        docker rm $containerId
-    fi
-
-    # 删除镜像
-    imageId=$(docker images | grep $dockerName | awk {'print $3'})
-    if [ ! -z "$imageId" ]; then
-        docker rmi -f $imageId
-    fi
-}
-cleanupDocker
+cleanupDocker $dockerName
 
 # 新建一个临时目录，用来放对应的Dockerfile来构建镜像
 tempDockerDir="temp_docker_build_$ngVersion"
@@ -49,39 +39,26 @@ fi
 
 # 基于创建的镜像，启动一个容器。同时将服务器上的 AWADE_BUILD_RESOURCE 目录挂载到容器中，方便获取编译资源
 docker run -v $AWADE_BUILD_RESOURCE:/data/awade-build/resource --name $dockerName ${imageId} &
-timeout=0
-while true; do
-    status=$(docker inspect $dockerName --format '{{.State.Status}}')
-    echo "$dockerName: 当前容器状态 >>>>>> status: $status"
-    if [ "$status" = "running" ]; then
-        break
-    fi
-    sleep 3
-    timeout=$((timeout + 3))
-    if [ $timeout -ge 60 ]; then
-        echo "$dockerName: 容器启动超时（60秒），当前状态: $status"
-        exit 1
-    fi
-done
+waitDockerStatus $dockerName
 
 containerId=$(docker ps -a | grep $dockerName | awk {'print $1'})
 echo "$dockerName: containerId >>>>>>>>>> $containerId ..."
 if [ -z "$containerId" ]; then
     echo "$dockerName: 未发现编译容器"
-    cleanupDocker
+    cleanupDocker $dockerName
     exit 1
 fi
 
 exitCode=$(docker wait $dockerName)
 echo "$dockerName: docker wait >>>>>>>>>> exitCode: $exitCode"
-docker cp $containerId:/data/build/build-in-docker-$ngVersion.log .
 if [ "$exitCode" != "0" ]; then
-    cleanupDocker
+    cleanupDocker $dockerName
     exit $exitCode
 fi
 
+docker cp $containerId:/data/build/build-in-docker-$ngVersion.log .
 if [ "$ngVersion" = "ng9" ]; then
-    cleanupDocker
+    cleanupDocker $dockerName
     exit 0
 fi
 
@@ -91,8 +68,8 @@ echo "$dockerName: cp dist from $containerId ..."
 docker cp $containerId:/data/build/dist .
 if [ "$?" != "0" ]; then
     echo "$dockerName: Error: docker cp $containerId:/data/build/dist ."
-    cleanupDocker
+    cleanupDocker $dockerName
     exit 1
 fi
 
-cleanupDocker
+cleanupDocker $dockerName
